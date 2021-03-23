5c12f2913cd58b5cd12b686e053b321919cb28f2
scottdomes <scottdomes@gmail.com>
2021-03-23 13:12:26 -0700

    Basic styles

--- components/course/CourseCard.js
+++ components/course/CourseCard.js
@@ -6,25 +6,25 @@ const CourseCard = (props) => {
   const { logo, price, title, description, progress, isOwned, link } = props;
   return (
     <Card className={styles.card}>
-      <div className={styles.container}>
-        <div className={styles.head}>
-          {logo}
-          <span className={styles.price}>${price}</span>
-        </div>
-        <h1 className={styles.title}>{title}</h1>
-        <p className={styles.description}>{description}</p>
-      </div>
-      {isOwned ? (
+      <Link href={`/courses/${link}`}>
         <div>
-          <h4>Progress: {progress}% complete</h4>
+          <div className={styles.container}>
+            <div className={styles.head}>
+              {logo}
+              {!isOwned && <span className={styles.price}>${price}</span>}
+            </div>
+            <h1 className={styles.title}>{title}</h1>
+            <p className={styles.description}>{description}</p>
+          </div>
+          {isOwned && (
+            <div className={styles.footer}>
+              <h4 className={styles.progress}>
+                Progress: {progress}% complete
+              </h4>
+            </div>
+          )}
         </div>
-      ) : (
-        <div className={styles.footer}>
-          <Link href={`/courses/${link}`}>
-            <button className={styles.button}>Learn more</button>
-          </Link>
-        </div>
-      )}
+      </Link>
     </Card>
   );
 };
--- stories/components/courses/CourseCard.stories.js
+++ stories/components/courses/CourseCard.stories.js
@@ -8,10 +8,22 @@ export default {
 
 const Template = (args) => <CourseCard {...args} />;
 
-export const Primary = Template.bind({});
-Primary.args = {
+export const Unowned = Template.bind({});
+Unowned.args = {
   logo: <img src="/tic_logo.svg" alt="Thinking in Code course logo" />,
   title: "Thinking in Code",
   price: 9.0,
+  link: "thinking-in-code",
+  description: "Start your coding journey by mastering universal principles",
+};
+
+export const Owned = Template.bind({});
+Owned.args = {
+  logo: <img src="/tic_logo.svg" alt="Thinking in Code course logo" />,
+  title: "Thinking in Code",
+  price: 9.0,
+  link: "thinking-in-code",
+  isOwned: true,
+  progress: 0,
   description: "Start your coding journey by mastering universal principles",
 };
