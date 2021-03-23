ba472b9eab42a581317ed3a318c96ae89cb94bd1
scottdomes <scottdomes@gmail.com>
2021-03-22 18:40:24 -0700

    Fix global css

--- components/Logo.js
+++ components/Logo.js
@@ -1,8 +1,7 @@
 import styles from "./styles/Logo.module.css";
 
 const Logo = () => {
-  console.log(styles)
-  return <h1 className={styles.text}>Volta Academy!</h1>;
+  return <h1 className={styles.text}>Volta Academy</h1>;
 };
 
 export default Logo;
