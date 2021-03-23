2c0d27835e686dd86be36c440d8173087cffe3b9
scottdomes <scottdomes@gmail.com>
2021-03-22 18:46:37 -0700

    Gradient

--- components/Logo.js
+++ components/Logo.js
@@ -1,7 +1,11 @@
 import styles from "./styles/Logo.module.css";
 
 const Logo = () => {
-  return <h1 className={styles.text}>Volta Academy</h1>;
+  return (
+    <h1 className={styles.text}>
+      <span className={styles.gradient}>Volta Academy</span>
+    </h1>
+  );
 };
 
 export default Logo;
