393126751b81bd2fef78dcb2fe8c53f33fbd842a
scottdomes <scottdomes@gmail.com>
2021-03-22 18:54:33 -0700

    Done card

---
+++ components/Card.js
@@ -0,0 +1,7 @@
+import styles from "./styles/Card.module.css";
+
+const Card = ({ children }) => {
+  return <div className={styles.container}>{children}</div>;
+};
+
+export default Card;
---
+++ stories/Card.stories.js
@@ -0,0 +1,16 @@
+import Card from "../components/Card";
+
+export default {
+  title: "Card",
+  component: Card,
+  argTypes: {
+    children: { control: <h1>Hello!</h1> },
+  },
+};
+
+const Template = (args) => <Card {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  children: <h1>Hello!</h1>,
+};
