bf06b3c641fbb5debfa80df56b95306a8be20310
scottdomes <scottdomes@gmail.com>
2021-03-23 16:42:50 -0700

    Done

--- pages/my/courses/[slug].js
+++ pages/my/courses/[slug].js
@@ -1,17 +1,18 @@
 import LessonCard from "components/course/LessonCard";
 import Layout from "components/layout/Layout";
 import { getCourse } from "pages/api/courses";
+import styles from "./styles/LessonIndex.module.css";
 
 const LessonIndex = ({ course }) => {
   const { slug, title, progress, lessons } = course;
   return (
     <Layout>
-      <div>
-        <div>
-          <img src={`/${slug}.svg`} alt="Course logo" />
-          <h1>{title}</h1>
+      <div className={styles.header}>
+        <div className={styles.title}>
+          <img className={styles.logo} src={`/${slug}.svg`} alt="Course logo" />
+          <h1 className={styles.titleText}>{title}</h1>
         </div>
-        <h3>{progress}% Complete</h3>
+        <h3 className={styles.progress}>{progress}% Complete</h3>
       </div>
       <hr />
       {lessons.map((lesson) => {
