862770eb80a2f5c48a2f642165c9db7235c0f86d
scottdomes <scottdomes@gmail.com>
2021-03-23 13:01:51 -0700

    Card button

--- components/course/CourseCard.js
+++ components/course/CourseCard.js
@@ -1,18 +1,28 @@
 import styles from "./styles/CourseCard.module.css";
 import Card from "../Card";
+import Link from "next/link";
 
-const CourseCard = ({ logo, price, title, description, progress }) => {
+const CourseCard = (props) => {
+  const { logo, price, title, description, progress, isOwned, link } = props;
   return (
-    <Card className={styles.container}>
-      <div className={styles.head}>
-        {logo}
-        <span className={styles.price}>${price}</span>
+    <Card className={styles.card}>
+      <div className={styles.container}>
+        <div className={styles.head}>
+          {logo}
+          <span className={styles.price}>${price}</span>
+        </div>
+        <h1 className={styles.title}>{title}</h1>
+        <p className={styles.description}>{description}</p>
       </div>
-      <h1 className={styles.title}>{title}</h1>
-      <p className={styles.description}>{description}</p>
-      {progress > 0 && (
+      {isOwned ? (
         <div>
           <h4>Progress: {progress}% complete</h4>
+        </div>
+      ) : (
+        <div className={styles.footer}>
+          <Link href={`/courses/${link}`}>
+            <button className={styles.button}>Learn more</button>
+          </Link>
         </div>
       )}
     </Card>
