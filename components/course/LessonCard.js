af18424810209754fc77a2aa2f572e193fb87367
scottdomes <scottdomes@gmail.com>
2021-03-26 17:11:12 -0700

    Very rough but going to merge and test

--- components/course/LessonCard.js
+++ components/course/LessonCard.js
@@ -6,12 +6,14 @@ import ProgressCircle from "./ProgressCircle";
 const LessonCard = ({ lesson, courseSlug }) => {
   return (
     <a href={`${courseSlug}/${lesson.slug}`}>
-      <Card className={styles.container}>
-        <h4>
-          Lesson {lesson.sequence}: {lesson.title}
-        </h4>
-        <ProgressCircle progress={lesson.progress} />
-      </Card>
+      <div>
+        <Card className={styles.container}>
+          <h4>
+            Lesson {lesson.sequence}: {lesson.title}
+          </h4>
+          <ProgressCircle progress={lesson.progress} />
+        </Card>
+      </div>
     </a>
   );
 };
